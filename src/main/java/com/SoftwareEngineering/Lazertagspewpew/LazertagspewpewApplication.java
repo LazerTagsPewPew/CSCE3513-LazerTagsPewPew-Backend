83b2eeaee309766be5845e2d900c5e62e0f15253
Clayton Warstler <claywarstler@gmail.com>
2023-02-07 14:26:29 -0600

    Created backend

---
+++ src/main/java/com/SoftwareEngineering/Lazertagspewpew/LazertagspewpewApplication.java
@@ -0,0 +1,13 @@
+package com.SoftwareEngineering.Lazertagspewpew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazertagspewpewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazertagspewpewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoftwareEngineering/Lazertagspewpew/LazertagspewpewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareEngineering.Lazertagspewpew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazertagspewpewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
